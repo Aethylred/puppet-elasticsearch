aeb0f4120f6c00daab6556edb0796bb64a64ff9e
Aaron Hicks <Aethylred@gmail.com>
2012-04-12 23:13:54 +1200

    righty, wrong package for CentOS

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,7 +10,8 @@ class elasticsearch(
     CentOS:{
       class{'elasticsearch::install':
         version       => $version,
-        install_root  => $install_root
+        install_root  => $install_root,
+        git_package => 'git'
       }
     }
     default:{
@@ -21,7 +22,8 @@ class elasticsearch(
 
 class elasticsearch::install(
   $version      = "0.19.2",
-  $install_root = "/opt"
+  $install_root = "/opt",
+  $git_package = "git-core"
 ){
   # NOTE: This is not a good way to install something.
   # It would be better to create RPM packages and put them in
@@ -29,10 +31,10 @@ class elasticsearch::install(
   # https://github.com/tavisto/elasticsearch-rpms
   # ...or use git to clone elasticsearch...
 
-  package{'git-core': ensure => installed}
+  package{$git_package: ensure => installed}
 
   exec{'install_elasticsearch':
-    require   => Package['git-core'],
+    require   => Package[$git_package],
     path      => ['/usr/bin'],
     cwd       => $install_root,
     user      => root,
