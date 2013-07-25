6f764eb13fc5e36e907eb6e8bbfcde3ea6a94855
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-07-25 12:26:11 +1200

    use head pattern for bigdesk

--- manifests/web/bigdesk.pp
+++ manifests/web/bigdesk.pp
@@ -1,12 +1,12 @@
-class elasticsearch::web::head (
+class elasticsearch::web::bigdesk (
   $apache_user = $elasticsearch::params::web_user,
   $web_root    = $elasticsearch::params::web_root,
   $web_group   = $elasticsearch::params::web_group,
 ) inherits elasticsearch::params {
 
-  vcsrepo{"${web_root}/elastichead":
+  vcsrepo{"${web_root}/bigdesk":
     ensure    => present,
-    source    => 'git://github.com/mobz/elasticsearch-head.git',
+    source    => 'git@github.com:lukas-vlcek/bigdesk.git',
     provider  => git,
     require   => Service['elasticsearch'],
   }
